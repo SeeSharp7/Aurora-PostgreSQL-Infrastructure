099aeb68601a0b6226f78db90f81b3b3905cfc82
René Fehlow <rene.fehlow@web.de>
2022-03-15 19:32:11 +0100

    Add simple aurora cluster on default vpc

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from '@aws-cdk/core';
+import * as rds from '@aws-cdk/aws-rds';
+import * as ec2 from '@aws-cdk/aws-ec2';
+
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const vpc = ec2.Vpc.fromLookup(this, "VPC", {
+      isDefault: true
+    });
+
+    const cluster = new rds.DatabaseCluster(this, 'Database', {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_13_4 }),
+      credentials: rds.Credentials.fromGeneratedSecret('clusteradmin'), // Optional - will default to 'admin' username and generated password
+      instanceProps: {
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MEDIUM),
+        vpc: vpc,
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PUBLIC,
+        }
+      },
+    });
+  }
+}
